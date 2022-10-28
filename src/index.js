f9db26d925c8d9119f2037d32fc3d6d4648948df
Mariana Mohr Silveira <marianamohr@MacBook-Air-de-Mariana-2.local>
2022-10-28 08:55:21 -0300

    Adicionando cors()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const { talker, login } = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
